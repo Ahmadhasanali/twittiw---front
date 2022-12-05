7b7342b9895b187f0bea918522a77549ce86e801
Ahmad Hasan Ali <ahmadhasanali24@student.uns.ac.id>
2022-12-05 21:59:32 +0700

    posts

---
+++ routes/posts.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const { Posts } = require('../models')
+const joi = require('joi')
+
+const router = express.Router()
+
+router.get('/posts', async(req, res) => {
+    const posts = await Posts.findAll()
+    const result = posts.map(post => {
+        return post
+    })
+
+    res.status(200).json({data: result})
+})
+
+const createPostSchema = joi.object({
+    content: joi.string().required()
+})
+
+router.post('/post', authMiddleware, async(req, res) => {
+    try {
+        const {content} = await createPostSchema.validateAsync(req.body)
+        const {user} = res.locals
+        const userId = user.dataValues.userId
+        const createPost = await Posts.create({
+            userId,
+            content
+        })
+        return res.status(201).json({
+            Status: '201',
+            Message: 'Created'
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(400).send({
+            Status: '400',
+            errorMessage: "The requested data type is not valid!"
+        })
+    }
+})
+
+router.post('/post/:idPost', authMiddleware, async(req, res) => {
+    const {user} = res.locals
+    const userId = user.dataValues.userId
+    const {idPost} = req.params
+    const post = await Posts.findOne({where:{postId: idPost}})
+
+    if (!post) {
+        return res.status(404).send({
+            Status: '404',
+            errorMessage: 'Data not found!'
+        })
+    }
+
+    if (userId !== post.userId) {
+        return res.status(403).send({
+            Status: '403',
+            errorMessage: 'Forbidden!'
+        })
+    }
+
+    if (post) {
+        await post.destroy()
+    }
+
+    res.status(200).send({
+        result: 'Success',
+        success: true
+    })
+})
+
+module.exports = router
